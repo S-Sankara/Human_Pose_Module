7475a40789d46816f7c3221ac1175355aae0314b
S-Sankara <65020263+S-Sankara@users.noreply.github.com>
2021-06-21 00:15:03 +0530

    Add files via upload

---
+++ Human_Pose.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+import mediapipe as mp
+import time
+
+class poseDetector():
+
+    def __init__(self, mode = False, upBody=False, smooth=True,
+                 detectionCon=0.5, trackingCon=0.5):
+
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackingCon = trackingCon
+
+        self.npDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
+                                self.detectionCon, self.trackingCon)
+
+    def findPose(self, img, draw = True):
+        img = cv2.resize(img, (800, 600))
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.npDraw.draw_landmarks(img, self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, draw=True):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                #print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return lmList
+
+
+def main():
+    cap = cv2.VideoCapture('test4.mp4')
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            print(lmList[14])
+            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 4,
+                    (225, 0, 0))
+        cv2.imshow('img', img)
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
+
